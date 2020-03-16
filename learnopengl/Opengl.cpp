3c18d87b97fb6a712046a69d7f861bce5005a765
Birat Kafle <60969078+biratkaf@users.noreply.github.com>
2020-03-16 22:57:28 +0545

    DOUBLE TROUBLR
    Triangle

--- learnopengl/Opengl.cpp
+++ learnopengl/Opengl.cpp
@@ -89,15 +89,30 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	float vertices[] = {
-		-0.5, -0.5, 0.0f, //left
-		 0.5f, -0.5f, 0.0f, //right
-		 0.0f, 0.5f, 0.0f  //top
+	float vertices [ ] = {
+		        
+		-0.2f,  0.2f, 0.0f,  // top 
+	    0.0f, -0.2f, 0.0f,  // bottom right
+	   -0.4f, -0.2f, 0.0f,  // bottom left
+
+		0.2f,  0.2f, 0.0f,  // top 
+		0.4f, -0.2f, 0.0f,  // bottom right
+	   0.0f, -0.2f, 0.0f,  // bottom left
+	   
+
 	};
 
-	unsigned int VBO, VAO;
+	/*unsigned int indices [ ] = {
+
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+
+	};*/
+
+	unsigned int VBO, VAO; //, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	//glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
@@ -106,7 +121,10 @@ int main()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices , GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -123,7 +141,9 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+
 
 
 
@@ -134,6 +154,7 @@ int main()
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	//glDeleteBuffers(1, &EBO);
 
 
 	glfwTerminate();
