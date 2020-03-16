7df54b9291efad3112e29791d37c993913c67a6c
Birat Kafle <60969078+biratkaf@users.noreply.github.com>
2020-03-17 01:07:41 +0545

    Double RECTROUBLE
    indices use

--- learnopengl/Opengl.cpp
+++ learnopengl/Opengl.cpp
@@ -23,6 +23,14 @@ const char* fragmentShaderSource = "#version 330 core\n"
      "     Fragcolor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
      "}\n\0";
 
+const char* fragmentShaderSource2 = "#version 330 core\n"
+"out vec4 Fragcolor; \n"
+"void main ()\n"
+"{\n"
+"     Fragcolor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+"}\n\0";
+
+
 
 
 
@@ -75,6 +83,21 @@ int main()
 		std::cout << "ERROR :: SHADER :: FRAGMENT :: COMPILATION FAILED\n" << infoLog << std::endl;
 	}
 
+
+
+	int fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+	glCompileShader(fragmentShader2);
+
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR :: SHADER :: FRAGMENT :: COMPILATION FAILED\n" << infoLog << std::endl;
+	}
+
+
+
 	int shaderProgram = glCreateProgram( );
 	glAttachShader(shaderProgram, vertexShader);
 	glAttachShader(shaderProgram, fragmentShader);
@@ -86,21 +109,61 @@ int main()
 		std::cout << "ERROR :: SHADER :: PROGRAM :: COMPILATION FAILED\n" << infoLog << std::endl;
 	}
 
+
+	int shaderProgram2 = glCreateProgram();
+	glAttachShader(shaderProgram2, vertexShader);
+	glAttachShader(shaderProgram2, fragmentShader2);
+	glLinkProgram(shaderProgram2);
+
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR :: SHADER :: PROGRAM :: COMPILATION FAILED\n" << infoLog << std::endl;
+	}
+
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+	glDeleteShader(fragmentShader2);
 
 	float vertices [ ] = {
 		        
-		-0.2f,  0.2f, 0.0f,  // top 
-	    0.0f, -0.2f, 0.0f,  // bottom right
-	   -0.4f, -0.2f, 0.0f,  // bottom left
+		0.0f,  0.2f, 0.0f,  // top right
+		  0.0f, -0.2f, 0.0f,  // bottom right
+		   -0.4f, -0.2f, 0.0f,  // bottom left
+		-0.4f,  0.2f, 0.0f,  // top left
 
-		0.2f,  0.2f, 0.0f,  // top 
-		0.4f, -0.2f, 0.0f,  // bottom right
-	   0.0f, -0.2f, 0.0f,  // bottom left
+	  
+	  
+
 	   
 
 	};
+
+
+	unsigned int indices [ ] = {
+
+	0, 1, 3,   // first triangle
+	1, 2, 3    // second triangle
+
+	};
+
+
+
+	float vertices2[] = {
+
+
+
+	0.4f,  0.2f, 0.0f,  // top right
+		0.4f, -0.2f, 0.0f,  // bottom right
+		   0.0f, -0.2f, 0.0f,  // bottom left
+	0.0f,  0.2f, 0.0f,  // top left
+
+
+
+
+	};
+
+
 
 	/*unsigned int indices [ ] = {
 
@@ -109,23 +172,44 @@ int main()
 
 	};*/
 
-	unsigned int VBO, VAO; //, EBO;
-	glGenVertexArrays(1, &VAO);
-	glGenBuffers(1, &VBO);
-	//glGenBuffers(1, &EBO);
-
-	glBindVertexArray(VAO);
+    unsigned int  VBOs [2];
+	unsigned int VAOs[2];
+    unsigned int EBO [2];
 
 
+	glGenVertexArrays(2,VAOs);
+	glGenBuffers(2, VBOs);
+	glGenBuffers(2, EBO);
 
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindVertexArray(VAOs[0]);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[0]);
+
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
+	
 	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices , GL_STATIC_DRAW);
 
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3* sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[0]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+	glBindVertexArray(VAOs[1]);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBOs[1]);
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices2, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 
@@ -140,9 +224,14 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		//glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT, 0);
-		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(VAOs[0]);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
+
+		glUseProgram(shaderProgram2);
+		glBindVertexArray(VAOs[1]);
+		glDrawElements(GL_TRIANGLES, 9,GL_UNSIGNED_INT, 0);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
 
 
 
@@ -152,8 +241,8 @@ int main()
 
 	}
 
-	glDeleteVertexArrays(1, &VAO);
-	glDeleteBuffers(1, &VBO);
+	glDeleteVertexArrays(2, VAOs);
+	glDeleteBuffers(2, VBOs);
 	//glDeleteBuffers(1, &EBO);
 
 
